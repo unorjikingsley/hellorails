133203492d21d19d6512f2266896bfd45d2ff92c
unorji kingsley <unorjikingsley@gmail.com>
2023-08-14 19:16:36 +0100

    connected to a db, made the file a read write accessible and change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#hello'
+
+  # find out the difference between the 'get' and 'roots' routes and also '/' and '#'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
